61147574932bea5e60c8718567bdaf3928af29f2
Gabriel Soares <gabriel@alfreddelivery.com>
2020-12-17 17:59:00 -0300

    Project init

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "senha",
+  database: "database",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
